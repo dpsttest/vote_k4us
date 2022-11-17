d135bb5213935c737c62c05c0ca3cd135a841c8f
dpsttest <90442179+dpsttest@users.noreply.github.com>
2022-11-17 20:19:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import chromedriver_autoinstaller
 chromedriver_autoinstaller.install()
 from datetime import datetime
@@ -27,24 +29,29 @@ print('Time start:', str(start_time))
 
 try:
     driver = webdriver.Chrome(options=chrome_options)
-    # driver = webdriver.Chrome() # run on your computer
+#     driver = webdriver.Chrome() # run on your computer
     driver.get('https://k4us.com.br/pak2022')
     print(driver.title)
+    driver_status = True
 except:
     print('Connection fails')
+    driver_status = False
+    driver.quit()
 
-for i in range(len(polls)):
-    element_vote = driver.find_element(By.ID, id_polls[i])
-    driver.execute_script("arguments[0].click();", element_vote)
-    element_submit = driver.find_element(By.CSS_SELECTOR, f"#{polls[i]} > form > .totalpoll-buttons > button.totalpoll-buttons-vote")
-    driver.execute_script("arguments[0].click();", element_submit)
-    time.sleep(10)
-    try: 
-        element_error = driver.find_element(By.CSS_SELECTOR, f"#{polls[i]} > div.totalpoll-message.totalpoll-message-error")
-        print(element_error.text)
-    except:
-        print('No error message')
-end_time = datetime.now()
-print('Time end:', str(end_time))
-print('Time difference:', str(end_time - start_time))
-driver.quit()
+if driver_status:
+    for i in range(len(polls)):
+        element_vote = driver.find_element(By.ID, id_polls[i])
+        driver.execute_script("arguments[0].click();", element_vote)
+        element_submit = driver.find_element(By.CSS_SELECTOR, f"#{polls[i]} > form > .totalpoll-buttons > button.totalpoll-buttons-vote")
+        driver.execute_script("arguments[0].click();", element_submit)
+        time.sleep(10)
+        try: 
+            print(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, f"#{polls[i]} > div.totalpoll-message.totalpoll-message-error"))).text)
+#             element_error = driver.find_element(By.CSS_SELECTOR, f"#{polls[i]} > div.totalpoll-message.totalpoll-message-error")
+#             print(element_error.text)
+        except:
+            print('No error message')
+    end_time = datetime.now()
+    print('Time end:', str(end_time))
+    print('Time difference:', str(end_time - start_time))
+    driver.quit()
