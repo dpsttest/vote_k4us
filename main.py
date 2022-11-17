c819769396b3059e2f8b317697608c10b68f29d6
dpsttest <90442179+dpsttest@users.noreply.github.com>
2022-11-17 20:35:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -46,9 +46,9 @@ if driver_status:
         driver.execute_script("arguments[0].click();", element_submit)
         time.sleep(10)
         try: 
-            print(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, f"#{polls[i]} > form > .totalpoll-message-error"))).text)
-#             element_error = driver.find_element(By.CSS_SELECTOR, f"#{polls[i]} > div.totalpoll-message.totalpoll-message-error")
-#             print(element_error.text)
+#             print(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, f"#{polls[i]} > form > .totalpoll-message-error"))).text)
+            element_error = driver.find_element(By.CSS_SELECTOR, f"#{polls[i]} > form > .totalpoll-message-error")
+            print(element_error.text)
         except:
             print('No error message')
     end_time = datetime.now()
